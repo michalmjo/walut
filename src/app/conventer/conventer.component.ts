6de49bc083d7fa0a33e4bfce1d05bd8573f1ab3b
michalmjo <michal.kmjo@gmail.com>
2022-10-25 14:29:39 +0200

    migrate css  to scss

--- src/app/conventer/conventer.component.ts
+++ src/app/conventer/conventer.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-conventer',
   templateUrl: './conventer.component.html',
-  styleUrls: ['./conventer.component.css']
+  styleUrls: ['./conventer.component.scss'],
 })
 export class ConventerComponent implements OnInit {
+  ammount = 1;
+  from = 'PLN';
+  to = 'USD';
+  rate = 0.21;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  conventer(): number {
+    return this.ammount * this.rate;
   }
-
 }
